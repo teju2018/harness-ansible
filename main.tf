c14e41b67ce3100f6599a7c085684d7cab1a2e1d
ganesh-redy <ganeshreddy7610@gmail.com>
2025-05-05 15:36:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+    project = "sam-458313"
+    zone = "us-west1-a"
+  
+}
+resource "google_compute_instance" "instance" {
+    name= "instance-1"
+    machine_type = "e2-standard-2"
+    boot_disk {
+      initialize_params {
+        image = "centos-stream-9"
+      }
+    }
+    network_interface {
+      network = "default"
+      access_config {
+        
+      }
+    }
+  
+}
+
+output "vm_external_ip" {
+    value =  google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip  
+}
