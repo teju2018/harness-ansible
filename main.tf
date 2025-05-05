08461edbdee270c77755c4455a9dc9a528dabc26
ganesh-redy <ganeshreddy7610@gmail.com>
2025-05-05 16:39:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,5 +21,5 @@ resource "google_compute_instance" "instance" {
 }
 
 output "vm_external_ip" {
-    value =  google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip  
+  value = google_compute_instance.instance.network_interface[0].access_config[0].nat_ip
 }
