f566d730efbf33541776d9de1d3d4bff127dc83b
ganesh-redy <ganeshreddy7610@gmail.com>
2025-05-07 16:43:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,29 @@
-provider "google" {
-    project = "sam-458313"
-    zone = "us-west1-a"
-  
+resource "tls_private_key" "my_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
-locals {
-PRIVATE_KEY = file("/root/.ssh/id_rsa")
-PUBLIC_KEY = file("/root/.ssh/id_rsa.pub")
-}
+
 resource "google_compute_instance" "instance" {
-    name= "instance-1"
-    machine_type = "e2-standard-2"
-    boot_disk {
-      initialize_params {
-        image = "centos-stream-9"
-      }
+  name         = "instance-1"
+  machine_type = "e2-standard-2"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-stream-9"
     }
-    network_interface {
-      network = "default"
-      access_config {
-        
-      }
-    }
-metadata = {
-  ssh-keys = "root:${local.PUBLIC_KEY}"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata = {
+    ssh-keys = "root:${tls_private_key.my_ssh_key.public_key_openssh}"
+  }
 }
 
-  
-}
-
-output "vm_external_ip" {
-  value = google_compute_instance.instance.network_interface[0].access_config[0].nat_ip
-}
 output "private_key" {
-  value     = local.PRIVATE_KEY
+  value     = tls_private_key.my_ssh_key.private_key_pem
   sensitive = true
 }
-
