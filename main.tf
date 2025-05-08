3556935f78c4a1ea1fffd0134e5150ae8bd70f65
ganesh-redy <ganeshreddy7610@gmail.com>
2025-05-08 20:57:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,13 @@
 provider "google" {
-  project = "sam-458313"        # ← your GCP project ID
-  region  = "us-west1"          # optional, for regional resources
-  zone    = "us-west1-a"        # for zonal resources like compute instances
-}
-
-
-
-resource "tls_private_key" "my_ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+  project = "sam-458313"
+  region  = "us-west1"
+  zone    = "us-west1-a"
 }
 
 resource "google_compute_instance" "instance" {
   name         = "instance-1"
   machine_type = "e2-standard-2"
-  zone = "us-west1-a"
+  zone         = "us-west1-a"
 
   boot_disk {
     initialize_params {
@@ -23,20 +16,15 @@ resource "google_compute_instance" "instance" {
   }
 
   network_interface {
-    network = "default"
+    network       = "default"
     access_config {}
   }
 
   metadata = {
-    ssh-keys = "ansible:${tls_private_key.my_ssh_key.public_key_openssh}"
+    ssh-keys = "ansible:${file("~/root/.ssh/id_rsa.pub")}"
   }
 }
 
-output "private_key" {
-  value     = tls_private_key.my_ssh_key.private_key_pem
-  sensitive = true
-}
 output "vm_external_ip" {
-   
   value = google_compute_instance.instance.network_interface[0].access_config[0].nat_ip
 }
